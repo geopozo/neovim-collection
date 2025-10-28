93beefea59e5b529dfa274fd33cadb0020046101
Andrew Pikul <ajpikul@gmail.com>
2025-10-28 09:39:35 -0400

    Improve markdown highlighting.

--- plugin/markdown_todo.lua
+++ plugin/markdown_todo.lua
@@ -1,8 +1,10 @@
 vim.treesitter.query.set("markdown_inline", "highlights", [[;extends
 
-  ((inline) @cancelled (#match? @cancelled "\\[c\\]") )
-  ((inline) @priority (#match? @priority "\\[ \\]\\*") )
-]]) -- had trouble adding this below
+  ((inline) @cancelled (#match? @cancelled "\\[c\\]") (#set! "priority" 130))
+  ((inline) @priority (#match? @priority "\\[ \\]\\*") (#set! "priority" 130))
+  ((inline) @done (#match? @done "\\[x\\]\\*") (#set! "priority" 130))
+  ((inline) @skipped (#match? @skipped "\\[s\\]") (#set! "priority" 130))
+]]) -- had trouble adding this below, but maybe it would work with priority
 
 vim.treesitter.query.set("markdown", "highlights", [[;extends
   (list_item
@@ -10,21 +12,13 @@ vim.treesitter.query.set("markdown", "highlights", [[;extends
     (task_list_marker_checked) @done
     (paragraph) @done
   )
-
-  (list_item
-    (list_marker_minus) @skipped
-    (paragraph
-      (inline) @skipped-txt
-    )
-    (#match? @skipped-txt "\\[s\\]")
-  )
 ]])
 
-vim.api.nvim_set_hl(0, "@skipped.markdown", { fg = "#DDDD00", strikethrough = true })
-vim.api.nvim_set_hl(0, "@skipped-txt.markdown", { fg = "#DDDD00", strikethrough = true })
+vim.api.nvim_set_hl(0, "@skipped.markdown_inline", { fg = "#DDDD00", strikethrough = true })
 vim.api.nvim_set_hl(0, "@cancelled.markdown_inline", { fg = "#ff5577", strikethrough = true })
 vim.api.nvim_set_hl(0, "@priority.markdown_inline",   { fg = "#ff1111" })
 vim.api.nvim_set_hl(0, "@done.markdown", { fg = "#00ff88" })
+vim.api.nvim_set_hl(0, "@done.markdown_inline", { fg = "#00ff88" })
 
 vim.treesitter.query.set("markdown", "folds", [[
   (section) @fold
