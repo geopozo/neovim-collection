66a86b65a2f743994d0162b7431b53d44b71c1ef
Andrew Pikul <ajpikul@gmail.com>
2025-11-10 11:17:37 -0500

    Copy in AI grammar.

--- plugin/tree-sitter-multipart/grammar.js
+++ plugin/tree-sitter-multipart/grammar.js
@@ -8,10 +8,36 @@
 // @ts-check
 
 module.exports = grammar({
-  name: "multipart",
+  name: 'multipart',
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    document: $ => seq(
+      repeat($.part),
+      $.final_boundary
+    ),
+
+    part: $ => seq(
+      $.boundary_line,
+      optional($.headers),
+      optional($.body)
+    ),
+
+    boundary_line: _ => token(seq('--BOUNDARY', /\r?\n/)),
+
+    final_boundary: _ => token(seq('--BOUNDARY--', /\r?\n?/)),
+
+    header_name: _ => /[A-Za-z0-9\-]+/,
+    header_value: _ => /[^\r\n]+/,
+    header: $ => seq(
+      field('name', $.header_name),
+      ':',
+      field('value', $.header_value),
+      /\r?\n/
+    ),
+
+    headers: $ => seq(repeat1($.header), /\r?\n/),
+
+    // Body is everything until next boundary line
+    body: _ => token(/[\s\S]*?(?=^--BOUNDARY(?:--)?\r?\n?)/m),
   }
 });
